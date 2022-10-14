fb242f95260e5c99463b22aaecc2dee22db63372
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-10-14 21:54:04 +0300

    Create Pokemoninfo.jsx

---
+++ src/components/Pokemoninfo.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDetailsData} from '../Redux/Details/Details'
+import { BallTriangle } from 'react-loader-spinner';
+
+
+
+function PokemonInfo() {
+  const  {data}   = useSelector((state) => state.details);
+  const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(true)
+  const pokeId = window.location.pathname.slice(13)
+  useEffect(() => {
+    dispatch(fetchDetailsData(pokeId));
+    setTimeout(() => {
+      setLoaded(false)
+    }, 3000)
+  }, [dispatch ,pokeId]);
+  
+
+
+  if (loaded) {
+    return (
+      <div className='loading'>
+      <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#4fa94d"
+        ariaLabel="ball-triangle-loading"
+        wrapperClass={{}}
+        wrapperStyle=""
+        visible={true}
+      />
+      </div>
+    );
+  }
+  return (
+    <>
+        <div key={data.id}>
+            <h2  className='pokename'>{data.name}</h2>
+
+            <img src={data.images.small} alt="img" className='pokeimg'/>
+            <ul className='pokelist'>
+            <li>
+              <p>Series :  {data.set.series}</p>
+            </li>
+            <li>
+              <p>Types :  {data.types[0]}</p>
+            </li>
+            <li>
+              <p>Rarity :  {data.rarity}</p>
+            </li>
+            <li>
+              <p>Artist :  {data.artist}</p>
+            </li>
+            <li>
+              <p>Cardmarket Average Selling Price :  {data.cardmarket.prices.averageSellPrice} $</p>
+            </li>
+          </ul>
+        </div>
+
+    </>
+  );
+}
+export default PokemonInfo;
+
