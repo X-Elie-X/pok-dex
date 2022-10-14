0d61d56fa6b0f228652e1cf8f10b2999638fc28a
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-10-14 21:53:15 +0300

    Create Homepage.js

---
+++ src/Redux/Homepage/Homepage.js
@@ -0,0 +1,24 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const url = `https://api.pokemontcg.io/v2/cards/`;
+
+const initialState = {
+  pokemons: [],
+};
+
+export const fetchData = createAsyncThunk('pokemons/fetchData', async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+});
+
+const pokemonSlice = createSlice({
+  name: 'pokemon',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchData.fulfilled]: (state, action) => action.payload,
+  },
+});
+
+export default pokemonSlice.reducer;
